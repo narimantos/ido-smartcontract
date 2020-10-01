3046882d6cb9a7aa624994821fec10453fb1b1e3
Nariman Etemadi <n.etemadi@intermax.nl>
2020-10-01 02:20:44 +0200

    added totalsupply function

--- app/script.js
+++ app/script.js
@@ -1,10 +1,5 @@
 const Web3 = require('web3');
-const Provider = require('@truffle/hdwallet-provider');
 const MyContract = require('../build/contracts/VisualWaifuToken.json');
-const address = '0x4744782cb53279A3aF254E6Ae4431A99Fd90Ab7c';
-const privateKey = '31b2704c8f5e10864d108f180b11671d46e260a9b33b3110817c64202a623334';
-
-
 
 const init1 = async (val) => {
     var web3 = new Web3("ws://192.168.2.10:8545");
@@ -25,9 +20,13 @@ const init1 = async (val) => {
         var name = await myContract.methods.name().call();
         var symbol = await myContract.methods.symbol().call();
         var balance = await myContract.methods.balanceOf(val).call();
-
-        var a = {'name':name, 'symbol':symbol, 'balance':balance};
-        return a
+        var totalSupply = await myContract.methods.totalSupply().call();
+        var a = {
+          'name':name,
+          'symbol':symbol,
+          'balance':balance,
+          'totalSupply': totalSupply
+         };return a
     }
     return userAccount;
   }
